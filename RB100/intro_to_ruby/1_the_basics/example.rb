4d1ec5744857cda6d57bfefd95a58f522838da9b
Ryan Parkerson <rparkerson@users.noreply.github.com>
2021-01-16 14:17:56 -0500

    Updates to chapter 0 and 1

---
+++ RB100/intro_to_ruby/1_the_basics/example.rb
@@ -0,0 +1,26 @@
+# examle.rb
+
+'Hello, world!'        # string literal
+375                    # integer literal
+3.141528               # float literal
+true                   # boolean literal
+{ 'a' => 1, 'b' => 2 } # hash literal
+[ 1, 2, 3 ]            # array literal
+:sym                   # symbol literal
+nil                    # nil literal
+
+# Ex. 1: with double quotes
+"The man said, 'Hi there!'"
+
+# Ex 2: with single quotes and escaping
+'The man said \'Hi there!\''
+
+# Examples of symbols
+
+:name
+:a_symbol
+:"surprisingly, this is also a symbol"
+
+# Example of integre literals
+1, 2, 3, 50, 10, 4345098098
+
---
+++ RB100/intro_to_ruby/1_the_basics/test.rb
@@ -0,0 +1,40 @@
+# test.rb
+require "pry"
+
+# movies = {
+#   movie_1: 1975,
+#   movie_2: 2004,
+#   movie_3: 2013,
+#   movie_4: 2001,
+#   movie_5: 1981
+# }
+
+# arr = movies.map { |title, year| year }
+
+# puts arr
+
+# puts arr[0]
+# puts arr[1]
+# puts arr[2]
+# puts arr[3]
+# puts arr[4]
+
+# puts 5 * 4 * 3 * 2 * 1
+# puts 6 * 5 * 4 * 3 * 2 * 1
+# puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+# puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# def factorial(num)
+#   (1..num).inject(:*) || 1
+# end
+
+# p factorial(5)
+# p factorial(0)
+
+# def squares(n)
+#   n * n
+# end
+
+# p squares(3.525)
+# p squares(4.23)
+# p squares(6.23)
