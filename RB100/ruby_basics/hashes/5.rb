f2340fab72f7f0490d2e1aaa7259b12d5426777d
Ryan Parkerson <rparkerson@users.noreply.github.com>
2021-01-26 12:53:01 -0500

    Completed hashes exercises

---
+++ RB100/ruby_basics/hashes/5.rb
@@ -0,0 +1,26 @@
+# Use Hash#each to iterate over numbers and print each element's 
+# key/value pair.
+
+# numbers = {
+#   high:   100,
+#   medium: 50,
+#   low:    10
+# }
+
+# Expected output:
+
+# A high number is 100.
+# A medium number is 50.
+# A low number is 10.
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+numbers.each { |k, v| puts "A #{k} number is #{v}" }
+
+numbers.each do |key, value|
+  puts "A #{key} number is #{value}"
+end
---
+++ RB100/ruby_basics/hashes/8.rb
@@ -0,0 +1,45 @@
+# In the following code, numbers isn't mutated because #select isn't 
+# a destructive method. However, there is a destructive version of 
+# #select named #select!. Modify the code to use #select! instead 
+# of #select.
+
+# numbers = {
+#   high:   100,
+#   medium: 50,
+#   low:    10
+# }
+
+# low_numbers = numbers.select do |key, value|
+#                  value < 25
+#                end
+
+# p low_numbers
+# p numbers
+
+# Expected output:
+
+# {low: 10}
+# {low: 10}
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+low_numbers = numbers.select! do |key, value|
+                 value < 25
+               end
+p low_numbers
+p numbers
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+low_numbers = numbers.select! { |key, value| value < 25 }  # mutates
+
+p low_numbers
+p numbers
