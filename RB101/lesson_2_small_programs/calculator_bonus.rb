5c75d1d9041d9e38d066a31ca7b729e2f31cbbbf
Ryan Parkerson <rparkerson@users.noreply.github.com>
2021-02-25 18:38:50 -0500

    Calculator_bonus and loan calculator small program updates

--- RB101/lesson_2_small_programs/calculator_bonus.rb
+++ RB101/lesson_2_small_programs/calculator_bonus.rb
@@ -118,7 +118,7 @@ loop do # main loop
            when '3'
              number1.to_i() * number2.to_i()
            when '4'
-             number1.to_f() + number2.to_f()
+             number1.to_f() / number2.to_f()
            end
 
   prompt("#{messages('result')} #{result}")
--- RB101/lesson_2_small_programs/loan_calculator.rb
+++ RB101/lesson_2_small_programs/loan_calculator.rb
@@ -42,28 +42,106 @@ return duration
 return monthly payment
 =end
 
-def payment_calculator(loan, interest, duration)
-  loan * (interest / (1 - (1 + interest)**(-duration)))
+# def payment_calculator(loan, interest, duration)
+#   (loan * (interest / (1 - (1 + interest)**(-duration)))).ceil(2)
+# end
+
+# puts("Welcome to the loan calculator!")
+
+# puts("What is the loan amount?")
+# loan_amount = gets.to_f
+
+# puts "What is the Anual Percentage Rate(APR)?"
+# apr = gets.to_f / 100
+
+# puts "What is the duration of the loan in months?"
+# duration_months = gets.to_f
+
+# monthly_interest = (apr / 12)
+# payment = payment_calculator(loan_amount, monthly_interest, duration_months)
+
+# p "Your loan amount is: #{loan_amount}"
+# p "Your apr is: #{apr}"
+# p "Your loan duration in months is: #{duration_months}"
+# p "--------------------"
+# p "Your monthly interest is: #{monthly_interest}"
+# p "Your loan duration in months is: #{duration_months}"
+# p "Your monthly payment is: #{payment}"
+
+#  Implimented input validation and optional repeat process
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-puts("Welcome to the loan calculator!")
+def payment_calculator(loan, interest, duration)
+  (loan * (interest / (1 - (1 + interest)**(-duration)))).ceil(2)
+end
 
-puts("What is the loan amount?")
-loan_amount = gets.to_f
+prompt("Welcome to the Loan Calculator!")
 
-puts "What is the Anual Percentage Rate(APR)?"
-apr = gets.to_f / 100
+loop do
+  prompt("What is the loan amount?")
 
-puts "What is the duration of the loan in months?"
-duration_months = gets.to_f
+  loan_amount = ''
+  loop do
+    loan_amount = gets().chomp()
 
-monthly_interest = apr / 12
-payment = payment_calculator(loan_amount, monthly_interest, duration_months)
+    if loan_amount.empty? || loan_amount.to_i < 0
+      prompt("Please enter a positive number.")
+    else
+      loan_amount = loan_amount.to_f
+      break
+    end
+  end
 
-p "Your loan amount is: #{loan_amount}"
-p "Your apr is: #{apr}"
-p "Your loan duration in months is: #{duration_months}"
-p "--------------------"
-p "Your monthly interest is: #{monthly_interest}"
-p "Your loan duration in months is: #{duration_months}"
-p "Your monthly payment is: #{payment}"
+  prompt("What is the Anual Percentage Rate(APR)?")
+  prompt("(Example: For 5% APR enter 5, for 2.5% APR enter 2.5)")
+
+  apr = ''
+  loop do
+    apr = gets().chomp()
+
+    if apr.empty? || apr.to_i < 0
+      prompt("Please enter a positive number.")
+    else
+      apr = apr.to_f / 100
+      break
+    end
+  end
+
+  prompt("What is the duration of the loan in years?")
+
+  duration_years = ''
+  duration_months = ''
+  loop do
+    duration_years = gets().chomp()
+
+    if duration_years.empty? || duration_years.to_i < 0
+      prompt("Please enter a positive number.")
+    else
+      duration_months = duration_years.to_i * 12
+      break
+    end
+  end
+
+  monthly_interest = (apr / 12)
+  payment = payment_calculator(loan_amount, monthly_interest, duration_months)
+
+  puts()
+  puts("--------------------")
+  prompt("Your loan amount is: $#{format('%.2f', loan_amount)}")
+  prompt("Your apr is: #{apr}")
+  prompt("Your loan duration in years is: #{duration_years}")
+  puts("--------------------")
+  prompt("Your monthly interest is: #{monthly_interest}")
+  prompt("Your loan duration in months is: #{duration_months}")
+  prompt("Your monthly payment is: $#{payment}")
+
+  prompt("Would you like to calculate another loan? (Y to repeat)")
+
+  answer = Kernel.gets().chomp().downcase()
+
+  break if answer != 'y' || answer != 'yes'
+end
+
+prompt("Thank you for using the loan calculator. Goodbye!")
