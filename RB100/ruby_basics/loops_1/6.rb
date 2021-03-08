cba81062ffb83fab07ab943bd00bb53e12f6d57c
Ryan Parkerson <rparkerson@users.noreply.github.com>
2021-03-08 16:37:03 -0500

    Added to exercises 6 and 8 in loops 1 Ruby Basics

--- RB100/ruby_basics/loops_1/6.rb
+++ RB100/ruby_basics/loops_1/6.rb
@@ -1,24 +1,60 @@
+=begin
+Using a while loop, print 5 random numbers between 0 and 99. 
+The code below shows an example of how to begin solving this exercise.
+
+numbers = []
+
+while <condition>
+  # ...
+end
+
+P 
+input: 
+output: numbers (5 random numbers)
+rules
+explicit:
+numbers between 0 and 99
+given format numbers = []
+implicit:
+use arrays
+
+questions:
+what is meant by between? inclusive
+
+Example output (your numbers will most likely be different):
+
+62
+96
+31
+16
+36
+
+D
+Arrays
+
+A
+initialize an empty array 'numbers'
+while 'numbers' array is < 5 elements in length
+  generate a number between 0 through 99
+  append number into 'numbers'
+  print all elements in numbers
+=end
+
 numbers = []
 
 while numbers.count < 5
   numbers.push(rand 100)
 end
-
 puts numbers
 
-
-
 numbers = []
+
 5.times {numbers.push(rand 100)}
 while numbers.count > 0
   p numbers.pop
 end
 
-
-
 5.times {p rand 100}
-
-
 
 numbers = []
 
@@ -26,10 +62,14 @@ while numbers.count < 5
  p numbers.unshift(rand 100).first
 end
 
-
-
 numbers = (0..99).to_a.shuffle
 
 while numbers.size > 95
   p numbers.pop
+end
+
+numbers = []
+
+while numbers.size < 5
+  numbers << p(rand 100)
 end
--- RB100/ruby_basics/loops_1/8.rb
+++ RB100/ruby_basics/loops_1/8.rb
@@ -17,7 +17,6 @@ until count > 4
   count += 1
 end
 
-
 numbers = [7, 9, 13, 25, 18]
 
 until false
@@ -33,18 +32,23 @@ until count == numbers.size
   count += 1
 end
 
-
 numbers = [7, 9, 13, 25, 18]
 
 until false do puts(numbers); break end
+# Better refactored version:
+break puts numbers until false
 
-# Not sure what is happening here
+# Not sure what is happening here(see below)
 numbers = [7, 9, 13, 25, 18]
 
 puts(numbers); break until false || true
-
-
+# numbers is printed on its own. ";" is like a next line command.
+# until then checks the logical or || and one is true so it evaluates to
+# true.  Since the until true condition is met it is not run and break is
+# never used.  if puts(numbers) was after break it would not execute either.
 
 numbers = [7, 9, 13, 25, 18]
 
 until numbers.length == 0 do puts numbers.shift end
+#  Refactored:
+puts numbers.shift until numbers.length == 0
