b68155f1ea23914c864421447d9c2392b029119b
Ryan Parkerson <rparkerson@users.noreply.github.com>
2021-03-25 21:03:00 -0400

    added features to tttbonus.rb

---
+++ RB101/lesson_6_slightly_larger_programs/tttbonus.rb
@@ -0,0 +1,242 @@
+require 'pry'
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+MATCHES_TO_WIN = 5
+WINNING_LINES = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+]
+MIDDLE_SQUARE = 5
+FIRST_MOVE = ['choose']
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def display_greeting
+  prompt "Welcome to Tic-Tac-Toe!"
+  prompt "Squares are numbered 1-9. The first to win 5 matches wins the game!"
+end
+
+def first_move_prompt
+  prompt "Choose who goes first? ('player' or 'computer')"
+  choose_first_move!
+end
+
+def choose_first_move!
+  choice = nil
+  loop do
+    choice = gets.chomp.downcase
+    if choice == 'p' || choice == 'player'
+      break choice = 'player'
+    elsif choice == 'computer' || choice == 'c'
+      break choice = 'computer'
+    else
+      prompt "Please choose either 'player' or 'computer' (p or c)."
+    end
+  end
+  FIRST_MOVE[0] = choice
+end
+
+# rubocop:disable Metrics/AbcSize
+def display_board(brd)
+  system 'clear'
+  puts "You are #{PLAYER_MARKER}'s. Computer is #{COMPUTER_MARKER}'s."
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+# rubocop:enable Metrics/AbcSize
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  new_board
+end
+
+def display_board_options
+  board_options = initialize_board
+  (1..9).each { |num| board_options[num] = num }
+  display_board(board_options)
+end
+
+def initialize_score
+  { 'Player' => 0, 'Computer' => 0 }
+end
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def joinor(arr, delimiter = ', ', word = 'or')
+  case arr.size
+  when 0..1 then arr.join
+  when 2 then arr.join(" #{word} ")
+  else
+    leading_str = arr[0, (arr.size - 2)].join(delimiter)
+    trailing_str = arr[-2, 2].join(" #{word} ")
+    leading_str + delimiter + trailing_str
+  end
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
+    square = gets.chomp
+    if square_valid?(square)
+      square = square.to_i
+      break if empty_squares(brd).include?(square)
+    end
+    prompt "Sorry, that's not a valid choice."
+  end
+  brd[square] = PLAYER_MARKER
+end
+
+def square_valid?(square)
+  square.to_i.to_s == square
+end
+
+def computer_places_piece!(brd)
+  square = nil
+
+  WINNING_LINES.each do |line|
+    square = find_at_risk_square(line, brd, COMPUTER_MARKER)
+    break if square
+  end
+
+  if !square
+    WINNING_LINES.each do |line|
+      square = find_at_risk_square(line, brd, PLAYER_MARKER)
+      break if square
+    end
+  end
+
+  square = MIDDLE_SQUARE if !square && brd[MIDDLE_SQUARE] == INITIAL_MARKER
+  square = empty_squares(brd).sample if !square
+
+  brd[square] = COMPUTER_MARKER
+end
+
+def find_at_risk_square(line, brd, marker)
+  board_values_line = brd.values_at(line[0], line[1], line[2])
+  if board_values_line.count(marker) == 2 &&
+     board_values_line.count(INITIAL_MARKER) == 1
+
+    return line[board_values_line.index(INITIAL_MARKER)]
+  end
+  nil
+end
+
+def computer_first_move(brd)
+  display_board(brd)
+  if FIRST_MOVE.first == 'computer'
+    computer_places_piece!(brd)
+  end
+end
+
+def board_full(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_match_winner(brd)
+end
+
+def detect_match_winner(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(line[0], line[1], line[2]).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+def display_match_winner(board)
+  if someone_won?(board)
+    prompt "#{detect_match_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+end
+
+def tally_score(score, winner)
+  score[winner] += 1 unless winner.nil?
+end
+
+def display_score(score)
+  prompt "Current Score: Player: #{score['Player']} Computer: \
+#{score['Computer']}"
+end
+
+def replay?
+  prompt "Press Enter to continue to the next round! (q to quit)"
+  choice = gets.chomp.downcase
+  !(choice == 'q' || choice == 'quit')
+end
+
+def game_winner?(score)
+  score['Player'] == MATCHES_TO_WIN || score['Computer'] == MATCHES_TO_WIN
+end
+
+def game_winner_prompt(score)
+  prompt "#{score.key(MATCHES_TO_WIN)} is the first to #{MATCHES_TO_WIN}\
+ and wins the game!"
+end
+
+def display_farewell
+  prompt "Thanks for playing! Goodbye!"
+end
+
+
+score = initialize_score
+display_board_options
+display_greeting
+first_move_prompt
+
+loop do
+  board = initialize_board
+  computer_first_move(board)
+
+  loop do
+    display_board(board)
+    
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full(board)
+    
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full(board)
+  end
+
+  display_board(board)
+  display_match_winner(board)
+
+  tally_score(score, detect_match_winner(board))
+  display_score(score)
+
+  break game_winner_prompt(score) if game_winner?(score)
+  break unless replay?
+end
+
+display_farewell
